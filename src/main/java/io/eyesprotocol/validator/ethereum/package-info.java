55b5646a245b71a183223afcd1f8abc23a4a384f
김용건 (Kim Younggeon) <mail@kyg.kr>
2022-01-03 15:36:31 +0900

    chore: add `package-info.java`

---
+++ src/main/java/io/eyesprotocol/validator/ethereum/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2022 EYES Protocol
+ * SPDX-Short-Identifier: LGPL-3.0-or-later
+ */
+
+/**
+ * Ethereum-related validation annotations and constraints
+ */
+package io.eyesprotocol.validator.ethereum;
